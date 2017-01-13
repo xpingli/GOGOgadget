b48c70e891d672e374e8cbd29d576e20183ff116
xpingli <xioli2013@gmail.com>
2017-01-12 17:13:22 -0700

    more options

--- R/GOGOpick.R
+++ R/GOGOpick.R
@@ -1,16 +1,18 @@
 #' GOGOpick automatically adds goTerms and other elements to the edgeR or DESeq2 processed file.
 #'
 #'
-#' @description  GOGOpick takes 4 arguments: "dataset" contains differentially expressed genes from edgeR and DESeq2. "database" is the Ontology database file you created use "DBconstruct" function. write" defualt is TRUE would write results in a csv file in the current directory. Use ... to specify the name of the csv file.
+#' @description  GOGOpick takes 6 arguments: "dataset" contains differentially expressed genes from edgeR and DESeq2. "database" is the Ontology database file you created use "DBconstruct" function. write" defualt is TRUE would write results in a csv file in the current directory. Use ... to specify the name of the csv file. Species is the full name of your organism, short is the short name for it. It also returns up regulation, down regulation, and a whole csv files.
 #'
 #' @author Xiaoping Li
 #'
 #' @examples \dontrun{GOGOpick("DE_drad_24hr_adjst.csv", [dra]OntologyDataBase, write = T, "Up and Down regulated genes by using DESeq2.csv")}
 #'
 #' @import dplyr
+#' @import biomartr
+#' @import KEGGREST
 #'
 #' @export
-GOGOpick <- function(dataset, database, write = TRUE, ... ){
+GOGOpick <- function(dataset, database, species, short, write = TRUE, ... ){
 
 
         Sig <- read.csv(dataset, stringsAsFactors = FALSE)
@@ -18,6 +20,7 @@ GOGOpick <- function(dataset, database, write = TRUE, ... ){
 
         Colnamez <- colnames(Sig)
         pattern <- "log2FoldChange"
+
 
         #DESeq2
         if(pattern %in% Colnamez){
@@ -28,7 +31,7 @@ GOGOpick <- function(dataset, database, write = TRUE, ... ){
 
                 colnames(up)[2] <- "logFC"
 
-                write.csv(up, "[dra][DES]upReg.csv")
+
 
                 down <-  Sig %>%
                         filter(padj <= 0.05 & log2FoldChange <= -1) %>%
@@ -36,24 +39,160 @@ GOGOpick <- function(dataset, database, write = TRUE, ... ){
                         mutate(DE = "-1")
 
                 colnames(down)[2] <- "logFC"
-                write.csv(down, "[dra][DES]downReg.csv")
+
 
 
         } else {
+
                 up <-   Sig %>%
                         filter(FDR <= 0.05 & logFC >= 1) %>%
                         select(refID, logFC) %>%
                         mutate(DE = "1")
 
-                write.csv(up, "[dra][edgeR]upReg.csv")
+
 
                 down <- Sig %>%
                         filter(FDR <= 0.05 & logFC <= -1) %>%
                         select(refID, logFC) %>%
                         mutate(DE = "-1")
 
-                write.csv(down, "[dra][edgeR]downReg.csv")
+
         }
+
+
+        #==============================================================
+
+        #library(KEGGREST)
+        #library(biomartr)
+
+        dra.proteome <- getProteome(db = "refseq", organism = species, path = file.path("_ncbi_downloads", "proteome"))
+
+        pro <- read_proteome(dra.proteome, format = "fasta")
+        pro.anno <- strsplit(pro@ranges@NAMES, ".1 ", fixed = T)
+
+        pro.index <- list(method = "vector")
+        for(i in 1:length(pro.anno)){
+                pro.index[i] <- pro.anno[[i]][1]
+        }
+
+        pro.index <- unlist(pro.index)
+
+        pro.annotation <- list(method = "vector")
+        for(i in 1:length(pro.anno)){
+                pro.annotation[i] <- pro.anno[[i]][2]
+        }
+
+        pro.annotation <- unlist(pro.annotation)
+
+        # set strings as characters
+        annotation_df <- data.frame(proteinID = pro.index, annotation = pro.annotation, stringsAsFactors = F)
+
+        ##===========================================================
+        ## Construct a dataset that can bridge proteinID and refID
+        kg <- keggConv(short, "ncbi-proteinid")
+        kg_split <- strsplit(names(kg), ":", fixed = TRUE)
+
+
+        ##protein ID
+        kg_proteinID <- list(method = "vector")
+        for(i in 1:length(kg)){
+                kg_proteinID[i] <- kg_split[[i]][2]
+        }
+        kg_proteinID <- unlist(kg_proteinID)
+
+        ##refID
+        kg_refID <- list(method = "vector")
+        for(i in 1:length(kg)){
+                split <- strsplit(kg, ":", fixed = TRUE)
+                kg_refID[i] <- split[[i]][2]
+        }
+
+        kg_refID <- unlist(kg_refID)
+
+        # set strings as characters
+        kg.index <- data.frame(refID = kg_refID, proteinID = kg_proteinID, stringsAsFactors = F)
+        ##========================================================
+
+        ## gene ID
+        geneid <- keggConv(short,"ncbi-geneid")
+
+
+        #x has to be a keggConv object
+        get_geneID <- function(x = geneid){
+                #x has to be a keggConv object
+                genex <- vector()
+                refx <- vector()
+
+                for ( i in 1:length(x)){
+                        genex[i] <- sapply(strsplit(names(x[i]), ":"), "[", 2)
+                        refx[i] <- sapply(strsplit(x[i], ":"), "[", 2)
+                }
+
+                data.frame(refID = refx, geneID = genex, stringsAsFactors = F)
+        }
+
+        kg.geneid <- get_geneID(geneid)
+
+###########################################################
+
+        construct <- function(x){
+
+                id_ensembl_x <- x$refID
+                id_ensembl_gene <- kg.geneid$refID
+                id_ensembl_pr <- kg.index$refID
+                anno_proid <- annotation_df$proteinID
+
+
+                Genid <- vector()
+                Proid <- vector()
+                Annos <- vector()
+
+                for(i in 1:nrow(x)){
+
+
+
+                        if(id_ensembl_x[i] %in% id_ensembl_gene){
+
+                                Genid[i] <- kg.geneid[kg.geneid$refID == id_ensembl_x[i], ]$geneID
+
+                        } else {
+                                Genid[i] <- "No match"
+                        }
+
+                        if(id_ensembl_x[i] %in% id_ensembl_pr){
+
+                                Proid[i] <- kg.index[kg.index$refID == id_ensembl_x[i], ]$proteinID
+
+                                if(Proid[i] %in% anno_proid){
+
+                                        Annos[i] <- annotation_df[annotation_df$proteinID == Proid[i], ]$annotation
+
+                                } else {
+
+                                        Annos[i] <- "No match"
+                                }
+
+                        } else {
+                                Proid[i] <- "No match"
+                        }
+
+
+                }
+
+                data.frame(refID = id_ensembl_x, geneID = Genid, proteinID = Proid, annotation = Annos, DE = x$DE, stringsAsFactors = F)
+
+        }
+
+        Up <- construct(up)
+        Down <- construct(down)
+
+        whole <- rbind(Up, Down)
+
+
+        write.csv(Up, "[dra]upReg.csv")
+        write.csv(Down, "[dra]downReg.csv")
+        write.csv(whole, "[dra]anno.csv")
+
 
 
 
