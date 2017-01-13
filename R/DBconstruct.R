8523ca9fa768ac3c6016a98e169a35145b369681
xpingli <xioli2013@gmail.com>
2017-01-12 23:40:56 -0700

    write.csv path

--- R/DBconstruct.R
+++ R/DBconstruct.R
@@ -296,7 +296,9 @@ DBconstruct <- function(ftp, species, short, write = F, ...){
 
         go <- tidy_db(constructed_db)
         if(write == TRUE){
-                write.csv(go, ...)
+
+                write.csv(go, file = paste("[GO]totalDB/", ..., sep = ""))
+
         } else {
                 warning("Write = F: not to produce a whole PANTHER Gene Ontology .csv file")
         }
