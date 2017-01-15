1177c504e6e76dd79750ff36de21999187469c97
xpingli <xioli2013@gmail.com>
2017-01-14 19:36:12 -0700

    IDmap problem

--- R/UniprotIDMaps.R
+++ R/UniprotIDMaps.R
@@ -1,13 +1,8 @@
-#' UniprotIDMaps data. dataset for mapping refID or gene ID to Uniprot ID, including symbols. Also loaded for GOGOscavenger function.
+#'UniprotIDMaps converts gene ID to Uniprot identifier
 #'
 #' @docType data
 #'
-#' @format data.frame
-#'
 #' @usage data("UniprotIDMaps")
 #'
-#'
-#' @references Uniprot
+#' @format A data frame
 "UniprotIDMaps"
-
-
